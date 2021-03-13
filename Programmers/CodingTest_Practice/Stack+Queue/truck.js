89529856ea5b942424d372ace19dbdb9553b04b5
MyeonuK <gusdn0217@gmail.com>
2021-03-14 01:25:16 +0900

    speedup

--- Programmers/CodingTest_Practice/Stack+Queue/truck.js
+++ Programmers/CodingTest_Practice/Stack+Queue/truck.js
@@ -6,29 +6,19 @@ function solution(bridge_length, weight, truck_weights) {
     let bridgeQueue = [[], []];
     let bridgeWeight = 0;
 
-    let truckNum = 0;
-    let truckWeight = 0;
-
-    for (let w in truck_weights) {
-        truckWeight += truck_weights[w];
-    }
-
-    while (bridgeWeight != 0 || truckWeight != 0) {
+    do {
         if (bridgeQueue[1][0] == 0) {
             bridgeWeight -= bridgeQueue[0].shift();
             bridgeQueue[1].shift();
-            truckNum--;
         }
 
-        if (truckWeight != 0 && bridgeWeight + truck_weights[0] <= weight && truckNum <= bridge_length) {
+        if (truck_weights[0] && bridgeWeight + truck_weights[0] <= weight && bridgeQueue[0].length <= bridge_length) {
             temp = truck_weights.shift();
             bridgeQueue[0].push(temp);
             bridgeQueue[1].push(bridge_length);
             bridgeWeight += temp;
-            truckWeight -= temp;
-            truckNum++;
         }
-        else if (truckWeight) {
+        else if (truck_weights[0]) {
             temp = bridgeQueue[1][0] - 1;
             for (let i in bridgeQueue[1]) {
                 bridgeQueue[1][i] -= temp;
@@ -41,7 +31,7 @@ function solution(bridge_length, weight, truck_weights) {
         }
 
         answer++;
-    }
+    } while (bridgeWeight)
 
     return answer;
 }
