9ea51cae5869127c3528787c494efd338c144786
MyeonuK <gusdn0217@gmail.com>
2021-03-16 22:49:05 +0900

    solving

---
+++ Programmers/CodingTest_Practice/Sort/biggest_number/biggest_number.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let answer = numbers.sort();
+
+    function mySort(arr) {
+
+    }
+    return numbers.sort().reverse().join("");
+}
+console.log(solution([6, 10, 2]));
+console.log(solution([3, 30, 34, 3415, 3400]));
+
