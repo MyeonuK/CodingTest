0e9f9796b9f861b8ad4218a8e8846d452f04a9bd
MyeonuK <gusdn0217@gmail.com>
2021-03-18 23:54:21 +0900

    failed

--- Programmers/CodingTest_Practice/Sort/biggest_number/biggest_number.js
+++ Programmers/CodingTest_Practice/Sort/biggest_number/biggest_number.js
@@ -1,11 +1,7 @@
 function solution(numbers) {
-    let answer = numbers.sort();
-
-    function mySort(arr) {
-
-    }
-    return numbers.sort().reverse().join("");
+    let answer = numbers.map(c => c + "").sort((a, b) => (b + a) - (a + b)).join("");
+    return answer[0] === '0' ? '0' : answer;
 }
-console.log(solution([6, 10, 2]));
-console.log(solution([3, 30, 34, 3415, 3400]));
 
+console.log(solution([6, 10, 2]));
+console.log(solution([3, 30, 34, 5, 9]));
